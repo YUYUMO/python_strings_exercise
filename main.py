4fd603fae0e3a774896a3bc411caaad3bea4de24
Y Y Mo <phyllis.yymo@gmail.com>
2025-07-01 16:18:45 +0100

    improve the logic of same_chars function

--- main.py
+++ main.py
@@ -19,12 +19,10 @@ hash_square(3)
 
 #Please write a function named same_chars, which takes one string and two integers as arguments. The integers refer to indexes within the string. The function should return True if the two characters at the indexes specified are the same. Otherwise, and especially if either of the indexes falls outside the scope of the string, the function returns False.
 def same_chars(string, integer1, integer2):
-    if len(string) < integer1 or len(string) < integer2:
-        return False
-    elif string[integer1] == string[integer2]:
-        return True
-    else:
-        return False
+    if len(string) > integer1 and len(string) > integer2:
+        if string[integer1] == string[integer2]:
+            return True
+    return False
 
 # same characters m and m
 print(same_chars("programmer", 6, 7)) # True
